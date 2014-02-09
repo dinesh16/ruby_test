5820d999b3e5b0b26cc61801fc1a44bb89dc4c58
Dinesh Anthony <dinesh.anthony@surfdome.com>
2014-02-09 16:08:32 +0000

    new brnch and file edited

--- test3.rb
+++ test3.rb
@@ -1,5 +1,19 @@
 def dd
-  puts 'testing git hhjhksdfhdshfsdf kjhsfdjkhsdjfhjfhdhfk'
+  puts 'testing git hhjhksdfhdshfsdf kjhsfdjkhsdjfhjfhdhfk'  # => nil
 end
 
-dd
+dd  # => nil
+
+
+class A
+
+  def show(a)
+    puts "this is value of a #{a}"  # => nil
+  end
+end
+
+a = A.new  # => #<A:0x007fe1d4094530>
+a.show(4)  # => nil
+
+# >> testing git hhjhksdfhdshfsdf kjhsfdjkhsdjfhjfhdhfk
+# >> this is value of a 4
